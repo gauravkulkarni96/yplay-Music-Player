c048f61dbad44b0e2777d2815fef917cf548110e
gauravkulkarni96 <gauravkulkarni96@gmail.com>
2018-06-10 13:41:41 +0530

    check existing songs

--- yplay.py
+++ yplay.py
@@ -1,40 +1,52 @@
 from __future__ import unicode_literals
 import youtube_dl
 import requests
-from bs4 import BeautifulSoup as bs
 import sys
 import re
+import os
 
-def download_song(link):
+DOWNLOAD_PATH = os.path.expanduser("~")+'/Documents/.yplay/'
+
+def download_song(video_id):
 	ydl_opts = {
+		'outtmpl': DOWNLOAD_PATH+'%(id)s.%(ext)s',
 	    'format': 'bestaudio/best',
 	    'postprocessors': [{
 	        'key': 'FFmpegExtractAudio',
 	        'preferredcodec': 'mp3',
 	        'preferredquality': '192',
 	    }],
-	    # 'outtmpl': '/home/gaurav/Desktop/file.mp4',
 	}
+	link = "https://www.youtube.com/watch?v="+video_id
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 	    ydl.download([link])
+	return
 
-def get_link(song_name):
+def get_video_id(song_name):
 	youtube_search_url = "https://www.youtube.com/results?search_query="+song_name
 
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 	page = requests.get(youtube_search_url, headers = headers)
-	# soup = bs((page._content), "html.parser")
-	# area = soup.findAll("a", {"id":"video-title"})
+
 	video_ids = re.findall(r"\"videoId\":\"\w+\"", str(page._content))[0]
 	video_id = video_ids.split(":")[1].strip("\"")
-	link = "https://www.youtube.com/watch?v="+video_id
-	return link
+	return video_id
 
+def search_songs(video_id):
+	return os.path.exists(DOWNLOAD_PATH+video_id+'.mp3')
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
-		print("Please pass the song name")
+		print("Please pass the song name\n")
 		exit(0)
 	song_name = "%20".join(sys.argv[1:])
-	link = get_link(song_name)
-	download_song(link)
+	video_id = get_video_id(song_name)
+	song_exists = search_songs(video_id)
+	if not song_exists:
+		try:
+			download_song(video_id)
+		except:
+			print("Could not download the song. Try again!\n")
+			exit(0)
+	command = "vlc "+DOWNLOAD_PATH+video_id+".mp3"
+	os.system(command)
